dd24fecfce939b2b8bde5bf99386a302f3f5d95a
Yoonus Arshaan <112463923+yoonusash@users.noreply.github.com>
2024-04-18 12:12:02 +0000

    Added additional information and animations

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -33,7 +33,15 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{
+            height: "3rem",
+            width: "3rem",
+            animation: "bounce 1s ease-in-out infinite alternate",
+          }}
+          alt={imageAltText}
+        />
       </div>
     </section>
   );
